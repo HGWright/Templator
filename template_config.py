d2b106d3e760c1656b1341af2442ae9b5c3d842c
Henry Wright <84939917+HGWright@users.noreply.github.com>
2025-03-14 13:48:05 +0000

    Create template_config.py (#39)

---
+++ template_config.py
@@ -0,0 +1,11 @@
+
+
+def repos_per_file_check(file_changed):
+  repos_per_file = {
+  "file_to_be_templated.yml": ["Templatee", "Templatee-2"]
+  "file_2": []
+  }
+  return repos_per_file(file_changed)
+
+if __name__ == "__main__":
+    main()
