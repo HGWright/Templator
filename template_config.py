066fabfa6f68679219a644fdd2889c4f3d3cc950
Henry Wright <84939917+HGWright@users.noreply.github.com>
2025-03-14 13:54:51 +0000

    Update template_config.py

--- template_config.py
+++ template_config.py
@@ -5,6 +5,7 @@ def repos_per_file_check(file_changed):
   "file_to_be_templated.yml": ["Templatee", "Templatee-2"]
   "file_2": []
   }
+  print(repos_per_file(file_changed))
   return repos_per_file(file_changed)
 
 if __name__ == "__main__":
